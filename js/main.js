bca7127fcd03ad6979b6dadfe1dfeaf436048b40
kipgeoffry <kipgeoffry@gmail.com>
2023-03-09 14:47:05 +0300

    updated logic to handle negative days on geDayOfWeek function

--- js/main.js
+++ js/main.js
@@ -1,39 +1,52 @@
+const femaleNames = ["Kwasi","Kwanwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
+const maleNames = ["Akosua","Adwao","Abenaa","Akua","Yaa","Afua","Ama",];
 
-function getPersonInfo(){
-    //event.preventDefault(); // prevent the form from submitting normally
-    // get the user input values
+function getGender(){
+    // get the user gender
     const gender = document.getElementById("gender").value;
+    console.log("Gender:", gender);   
+    return gender;      
+}
+function getBirthDate(){
+    // get the user birthday
     const birthDay = document.getElementById("birthdate").value;
-    // do something with the values, such as display them
-    if (birthDay == null){
-        alert("Enter a valid Birthdate")
-    }
     console.log("Birthday:", birthDay);
-    console.log("Gender:", gender);           
+    return birthDay;
 }
 function getDayOfWeek(){
-    const cc = Number(birthDate.slice(0,2));
-    const yy = Number(birthDate.slice(2,4));
-    const mm = Number(birthDate.slice(5,7));
-    const dd = Number(birthDate.slice(-2));
-    let d = Math.floor(( ( (cc/4) -2*cc-1) + ((5*yy/4) ) + ((26*(mm+1)/10)) + dd ) % 7);
-    return d
+    let bornDay = getBirthDate();
+    const cc = Number(bornDay.slice(0,2));
+    const yy = Number(bornDay.slice(2,4));
+    const mm = Number(bornDay.slice(5,7));
+    const dd = Number(bornDay.slice(-2));
+    let d = ( ((cc/4) -2*cc-1) + ((5*yy/4) ) + ((26*(mm+1)/10)) + dd ) % 7;
+    let day = null;
+    if (d < 0){
+        day = Math.floor(d*-1)
+    }else{
+        day = Math.floor(d)
+    }
+    console.log(day);
+    return day;
+    
 }
 
 function getAkanName(){
-    let day = null;
-    if (gender == "male"){
-        day = maleNames[getDayOfWeek()];
+    let sex = getGender();
+    let akanName = null;
+    if (sex == "male"){
+        akanName = maleNames[getDayOfWeek()];
+               
+    } else {
+        akanName = femaleNames[getDayOfWeek()];
         
-    } 
-    else {
-        day = femaleNames[getDayOfWeek()]
     }
-    return day
+    console.log(akanName)
+    return akanName
+    
 }
 
 function main(){
-    console.log(getAkanName())
+    document.getElementById("output").innerHTML = "Your Akan Name is: " + getAkanName();
 }
 
-main()
